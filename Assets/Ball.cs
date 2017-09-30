f08084796efda1837eeb2f1c7e52f054f5b4bdce
Gaurav <the1337est@live.com>
2017-09-29 20:56:00 -0400

    -Added basic movements

---
+++ Assets/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Ball : MonoBehaviour
+{
+
+    public float Speed;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        Fire();
+	}
+
+    void Fire()
+    {
+        Vector2 dir = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+        dir = Vector2.ClampMagnitude(dir, Speed);
+        rb.velocity = dir;
+    }
+	
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType
+{
+    PlayerOne,
+    PlayerTwo
+}
+
+public class Player : MonoBehaviour
+{
+
+    public PlayerType Type;
+
+    public float Speed;
+
+	void Update ()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (Type == PlayerType.PlayerTwo)
+            {
+                transform.position += Vector3.up * Time.deltaTime * Speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (Type == PlayerType.PlayerTwo)
+            {
+                transform.position += Vector3.down * Time.deltaTime * Speed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Type == PlayerType.PlayerOne)
+            {
+                transform.position += Vector3.up * Time.deltaTime * Speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Type == PlayerType.PlayerOne)
+            {
+                transform.position += Vector3.down * Time.deltaTime * Speed;
+            }
+        }
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.69f, 3.69f), 0f);
+
+    }
+}
