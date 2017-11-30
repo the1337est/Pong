a8402fd686a340eda868fe1928a27b272e42f5d9
Gaurav <gauravtripathi198@gmail.com>
2017-11-29 23:32:29 -0500

    -Implemented scoring and gameover condition

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class Ball : MonoBehaviour
@@ -19,5 +20,29 @@ public class Ball : MonoBehaviour
         dir = Vector2.ClampMagnitude(dir, Speed);
         rb.velocity = dir;
     }
-	
+
+    public void Restart()
+    {
+        if (!GameManager.Instance.Over)
+        {
+            transform.position = Vector3.zero;
+            Fire();
+        }
+    }
+
+    private void Update()
+    {
+        if (!GameManager.Instance.Over)
+        {
+            if (transform.position.x < -6f)
+            {
+                GameManager.Instance.Score2 = GameManager.Instance.Score2 + 1;
+            }
+            if (transform.position.x > 6f)
+            {
+                GameManager.Instance.Score1 = GameManager.Instance.Score1 + 1;
+            }
+        }
+    }
+
 }
---
+++ Assets/GameManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    public Text Player1Score;
+    public Text Player2Score;
+    public Text Result;
+
+    public int MaxScore;
+
+    int score1;
+    int score2;
+
+    Ball ball;
+    public bool Over = false;
+
+    public int Score1
+    {
+        get
+        {
+            return score1;
+        }
+        set
+        {
+            score1 = value;
+            Player1Score.text = score1.ToString();
+            if (score1 == MaxScore)
+            {
+                Over = true;
+                Gameover(1);
+            }
+            else
+            {
+                ball.Restart();
+            }
+        }
+    }
+
+    public int Score2
+    {
+        get
+        {
+            return score2;
+        }
+        set
+        {
+            score2 = value;
+            Player2Score.text = score2.ToString();
+            if (score2 == MaxScore)
+            {
+                Over = true;
+                Gameover(2);
+            }
+            else
+            {
+                ball.Restart();
+            }
+        }
+    }
+
+
+    void Awake ()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(Instance);
+            }
+        }
+
+        ball = FindObjectOfType<Ball>();
+	}
+
+    private void Update()
+    {
+        if (Over)
+        {
+            if (Input.anyKeyDown)
+            {
+                Reset();
+            }
+        }
+    }
+
+    public void AddScore(PlayerType pType, int points)
+    {
+        switch (pType)
+        {
+            case PlayerType.PlayerOne:
+                score1 += points;
+                break;
+            case PlayerType.PlayerTwo:
+                score2 += points; 
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Gameover(int player)
+    {
+        Over = true;
+        if (player == 1)
+        {
+            Result.text = "Player 1 Wins!";
+        }
+        else
+        {
+            Result.text = "Player 2 Wins!";
+        }
+    }
+
+    private void Reset()
+    {
+        Over = false;
+        Score1 = 0;
+        Score2 = 0;
+        ball.Restart();
+        Result.text = "";
+    }
+
+}
